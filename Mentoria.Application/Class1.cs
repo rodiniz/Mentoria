fccb9055f32e6557863e86c102a73bc0054670d1
Rodrigo DINIZ <rodrigo.diniz@s-money.net>
2023-04-04 16:55:56 +0200

    Enhance project

--- Mentoria.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Mentoria.Application
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Mentoria.Infrastructure/RegisterServices.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mentoria.Infrastructure
+{
+    public static class RegisterServices
+    {
+        public static void ConfigureInfraStructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(configuration.GetConnectionString("MyConnection"));
+            });
+        }
+    }
+}
